c8e99355f3fe1dfd7598799bfc1f72f306178dd9
kimbs <vlakd0711@naver.com>
2019-08-12 13:50:34 +0900

    오늘은 여기까지.

---
+++ authentication-service/src/main/java/org/kimbs/authenticationservice/security/WebSecurityConfigurer.java
@@ -0,0 +1,38 @@
+package org.kimbs.authenticationservice.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class WebSecurityConfigurer extends WebSecurityConfigurerAdapter {
+
+    @Override
+    @Bean
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    @Override
+    @Bean
+    public UserDetailsService userDetailsServiceBean() throws Exception {
+        return super.userDetailsServiceBean();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        PasswordEncoder passwordEncoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+
+        auth
+                .inMemoryAuthentication()
+                .passwordEncoder(passwordEncoder)
+                .withUser("kimbs").password(passwordEncoder.encode("password1")).roles("USER", "ADMIN")
+                .and()
+                .withUser("william.woodward").password(passwordEncoder.encode("password2")).roles("USER");
+    }
+}
