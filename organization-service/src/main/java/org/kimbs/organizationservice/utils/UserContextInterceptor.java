8f9f54313f01d8f957461ce73e3501322751a7f0
kimbs <vlakd0711@naver.com>
2019-08-27 16:26:40 +0900

    Cleanup organization service

--- organization-service/src/main/java/org/kimbs/organizationservice/utils/UserContextInterceptor.java
+++ organization-service/src/main/java/org/kimbs/organizationservice/utils/UserContextInterceptor.java
@@ -13,6 +13,7 @@ public class UserContextInterceptor implements ClientHttpRequestInterceptor {
     @Override
     public ClientHttpResponse intercept(HttpRequest httpRequest, byte[] bytes, ClientHttpRequestExecution clientHttpRequestExecution) throws IOException {
         HttpHeaders headers = httpRequest.getHeaders();
+
         headers.add(UserContext.CORRELATION_ID, UserContextHolder.getContext().getCorrelationId());
         headers.add(UserContext.AUTH_TOKEN, UserContextHolder.getContext().getAuthToken());
 
